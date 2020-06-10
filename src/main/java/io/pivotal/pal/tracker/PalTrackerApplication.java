02383460cdc68d0db05d1caba34c08f244f8b00b
EshwarMV <emuthyam@ford.com>
2020-06-10 19:32:02 +0000

    JDBC Connection

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -4,10 +4,13 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.mysql.cj.jdbc.MysqlDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class PalTrackerApplication {
@@ -18,6 +21,9 @@ public class PalTrackerApplication {
 
     @Bean
     TimeEntryRepository timeEntryRepository() {
-        return new InMemoryTimeEntryRepository();
+        MysqlDataSource dataSource = new MysqlDataSource();
+        dataSource.setUrl(System.getenv("SPRING_DATASOURCE_URL"));
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+        return new JdbcTimeEntryRepository(dataSource);
     }
 }
--- src/main/java/io/pivotal/pal/tracker/TimeEntryRepository.java
+++ src/main/java/io/pivotal/pal/tracker/TimeEntryRepository.java
@@ -1,11 +1,12 @@
 package io.pivotal.pal.tracker;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public interface TimeEntryRepository {
-    TimeEntry create(TimeEntry timeEntry);
+    TimeEntry create(TimeEntry timeEntry) throws SQLException;
     TimeEntry find(Long id);
-    List<TimeEntry> list();
+    List<TimeEntry> list() throws SQLException;
     TimeEntry update(Long id, TimeEntry timeEntry);
     void delete(Long id);
 }
